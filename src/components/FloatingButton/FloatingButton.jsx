5a5729fb6f90c135b59a6b07c0a20d287e382549
Lucas Cardoso <lucascardosoprogramador@gmail.com>
2024-11-18 23:29:26 -0300

    bug gtm

--- src/components/FloatingButton/FloatingButton.jsx
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -3,22 +3,26 @@ import { FaWhatsapp } from 'react-icons/fa';
 import './floatingButton.css';
 
 const FloatingButton = () => {
-  const handleClick = (e) => {
-    e.preventDefault();
-    const url = "https://api.whatsapp.com/send?phone=5513991103209&text=Ol%C3%A1%2C%20Cl%C3%ADnica%20Violetta!%20Eu%20preciso%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20seus%20servi%C3%A7os%20veterin%C3%A1rios.";
-    setTimeout(() => {
-      window.open(url, '_blank', 'noopener noreferrer');
-    }, 300); // Atraso de 300ms
-  };
-
   return (
     <div className="floating-container">
       <div className="floating-message">
         <span>Precisa de ajuda? <br /> Fale conosco no WhatsApp!</span>
       </div>
-      <button onClick={handleClick} className="floating-btn">
+      <a
+        href="https://api.whatsapp.com/send?phone=5513991103209&text=Ol%C3%A1%2C%20Cl%C3%ADnica%20Violetta!%20Eu%20preciso%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20seus%20servi%C3%A7os%20veterin%C3%A1rios."
+        target="_blank"
+        rel="noopener noreferrer"
+        className="floating-btn"
+        onClick={(e) => {
+          e.preventDefault(); // Previne o redirecionamento imediato
+          const url = e.currentTarget.href; // Pega o URL do href
+          setTimeout(() => {
+            window.open(url, '_blank', 'noopener noreferrer');
+          }, 300); // Redireciona após 300ms
+        }}
+      >
         <FaWhatsapp className="whatsapp-icon" />
-      </button>
+      </a>
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,14 +4,6 @@ import { FaWhatsapp } from 'react-icons/fa'; // Importar o ícone do WhatsApp
 import videoSource from '/src/assets/teste.mp4'; // Importar o vídeo corretamente
 
 const Home = () => {
-  const handleWhatsAppClick = (e) => {
-    e.preventDefault();
-    const url = "https://api.whatsapp.com/send?phone=5513991103209&text=Ol%C3%A1%2C%20Cl%C3%ADnica%20Violetta!%20Eu%20preciso%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20seus%20servi%C3%A7os%20veterin%C3%A1rios.";
-    setTimeout(() => {
-      window.open(url, '_blank', 'noopener noreferrer');
-    }, 300); // Atraso de 300ms
-  };
-
   return (
     <section className="home">
       <video className="background-video" autoPlay loop muted playsInline>
@@ -21,13 +13,23 @@ const Home = () => {
       {/* Seção de conteúdo à esquerda */}
       <div className="home-content">
         <div className="home-buttons">
-          <button 
-            className="btn-primary" 
-            onClick={handleWhatsAppClick}
+          <a
+            href="https://api.whatsapp.com/send?phone=5513991103209&text=Ol%C3%A1%2C%20Cl%C3%ADnica%20Violetta!%20Eu%20preciso%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20seus%20servi%C3%A7os%20veterin%C3%A1rios."
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={(e) => {
+              e.preventDefault(); // Previne o redirecionamento imediato
+              const url = e.currentTarget.href; // Pega o URL do href
+              setTimeout(() => {
+                window.open(url, '_blank', 'noopener noreferrer');
+              }, 300); // Redireciona após 300ms
+            }}
           >
-            <FaWhatsapp style={{ marginRight: '10px' }} />
-            Fale com a Veterinária
-          </button>
+            <button className="btn-primary">
+              <FaWhatsapp style={{ marginRight: '10px' }} />
+              Fale com a Veterinária
+            </button>
+          </a>
           {/* Link para a seção de serviços */}
           <a href="#services">
             <button className="btn-secondary">Nossos Serviços</button>
