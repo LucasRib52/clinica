e01369f9bbc0363ad13c153e0429d716f74da93d
Lucas Cardoso <lucascardosoprogramador@gmail.com>
2024-11-18 23:06:35 -0300

    alterando link do botao flutuante de clique wpp

--- src/components/FloatingButton/FloatingButton.jsx
+++ src/components/FloatingButton/FloatingButton.jsx
@@ -9,7 +9,7 @@ const FloatingButton = () => {
         <span>Precisa de ajuda? <br /> Fale conosco no WhatsApp!</span>
       </div>
       <a
-        href="https://wa.me/5513991103209"
+        href="https://api.whatsapp.com/send?phone=5513991103209&text=Ol%C3%A1%2C%20Cl%C3%ADnica%20Violetta!%20Eu%20preciso%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20os%20seus%20servi%C3%A7os%20veterin%C3%A1rios."
         target="_blank"
         rel="noopener noreferrer"
         className="floating-btn"
